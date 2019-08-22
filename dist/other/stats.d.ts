9628dd31cfd55c80016238c4bf2a614cff0b3b29
terncode <52113450+terncode@users.noreply.github.com>
2019-08-23 00:07:17 +0200

    Delete stats.d.ts

--- dist/other/stats.d.ts
+++
@@ -1,5 +0,0 @@
-import { Message } from 'discord.js';
-export declare function stats(message: Message): boolean | void;
-export declare function formatDate(date: Date): string;
-export declare function getAge(date: Date, lang?: any): string;
-export declare function botInfo(lang: any, message: Message): void;
