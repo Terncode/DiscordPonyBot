203b797be382b2189a0360938af540fb71b20673
terncode <52113450+terncode@users.noreply.github.com>
2019-08-22 23:37:46 +0200

    Delete main.d.ts

--- dist/main.d.ts
+++
@@ -1,5 +0,0 @@
-import { Message, Guild, GuildMember } from 'discord.js';
-export declare function onMessage(message: Message): Promise<Message | Message[]>;
-export declare function clientGuildJoin(guild: Guild): Promise<void>;
-export declare function onGuildJoin(member: GuildMember): void;
-export declare function onGuildLeave(member: GuildMember): void;
