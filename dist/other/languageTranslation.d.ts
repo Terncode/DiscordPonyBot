4cf3126e4c6ccaede17a92d03ea24e9744164737
terncode <52113450+terncode@users.noreply.github.com>
2019-08-22 23:53:38 +0200

    Delete languageTranslation.d.ts

--- dist/other/languageTranslation.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function languageTranslation(lang: string): Promise<unknown>;
-export declare function isSupported(language: any): boolean;
-export declare function getISOCode(language: any): any;
